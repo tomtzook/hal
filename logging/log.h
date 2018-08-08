ebb162f9622b865d9e784705428c34ade8314549
Tom Tzook <tomtzook@gmail.com>
2018-08-08 23:24:38 +0300

    added logging

---
+++ logging/log.h
@@ -0,0 +1,13 @@
+#ifndef HAL_LOG_H
+#define HAL_LOG_H
+
+#include <stdio.h>
+
+#define LOG(X...) \
+    printf(X...)
+
+#define LOGLN(X...) \
+    LOG(X...); \
+    printf("\n");
+
+#endif //HAL_LOG_H
